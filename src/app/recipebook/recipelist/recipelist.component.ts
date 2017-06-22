c022757d44e471752afcaadb0f38304646e91925
James Wiggins <james.s.wiggins@gmail.com>
2017-06-22 16:59:17 +0000

    test

--- src/app/recipebook/recipelist/recipelist.component.ts
+++ src/app/recipebook/recipelist/recipelist.component.ts
@@ -11,7 +11,8 @@ export class RecipelistComponent implements OnInit {
 	recipes: Recipe[] = [
 		new Recipe('A Test Recipe', 'This is a simply a test.', 'http://2.bp.blogspot.com/-UXaKfAnAWRY/TbZ7Jtz5g8I/AAAAAAAALbs/0Sz83Nr5oxQ/s1600/recipe+clipart.gif'),
 		new Recipe('A Test Recipe 3', 'This is a simply a test.', 'http://2.bp.blogspot.com/-UXaKfAnAWRY/TbZ7Jtz5g8I/AAAAAAAALbs/0Sz83Nr5oxQ/s1600/recipe+clipart.gif'),
-		new Recipe('A Test Recipe 4', 'This is a simply a test.', 'http://2.bp.blogspot.com/-UXaKfAnAWRY/TbZ7Jtz5g8I/AAAAAAAALbs/0Sz83Nr5oxQ/s1600/recipe+clipart.gif')
+		new Recipe('A Test Recipe 4', 'This is a simply a test.', 'http://2.bp.blogspot.com/-UXaKfAnAWRY/TbZ7Jtz5g8I/AAAAAAAALbs/0Sz83Nr5oxQ/s1600/recipe+clipart.gif'),
+		new Recipe('A Test Recipe 5', 'This is a simply a test.', 'http://2.bp.blogspot.com/-UXaKfAnAWRY/TbZ7Jtz5g8I/AAAAAAAALbs/0Sz83Nr5oxQ/s1600/recipe+clipart.gif')
 	];
 
   constructor() { }
